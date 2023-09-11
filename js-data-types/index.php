a12ab1b0c90c2dae0ef4985930193f30e2950cd5
labsan <yondeveloper@gmail.com>
2023-09-12 03:21:56 +0700

    add js-data-types

---
+++ js-data-types/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    // Type data is Number
+    let length = 16; 
+
+    // Type data is String
+    let lastName = "Johnson";
+
+    // Type data is Object
+    const x = {
+        firstName   : "John",
+        lastName    : "Doe"
+    };
+
+?>
