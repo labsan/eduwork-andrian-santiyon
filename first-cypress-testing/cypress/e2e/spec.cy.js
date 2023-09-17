de0bcff7fe59e206a87baa1501a33e2557c3abc1
labsan <yondeveloper@gmail.com>
2023-09-17 15:56:30 +0700

    add first test script

--- first-cypress-testing/cypress/e2e/spec.cy.js
+++ first-cypress-testing/cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('My First Test', () => {
+  it('Visits the Kitchen Sink', () => {
     cy.visit('https://example.cypress.io')
+  
+    // Query for 'get' element with 'click' action
+    cy.contains('type').click()
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it and verify that the value has been updated
+    cy.get('.action-email').type('yonsantiyon@gmail.com').should('have.value', 'yonsantiyon@gmail.com')
   })
 })
