4bb9ce27003e592f06fa48621d3915091067d0cf
labsan <yondeveloper@gmail.com>
2023-09-20 18:43:34 +0700

    update login test

--- first-cypress-testing/cypress/e2e/login.cy.js
+++ first-cypress-testing/cypress/e2e/login.cy.js
@@ -1,2 +1,23 @@
 /// <reference types="cypress" />
 
+describe('Working with inputs', () => {
+   it('Should load the website', () => {
+       cy.visit('http://zero.webappsecurity.com/login.html');
+       cy.url().should('include', 'login.html');
+   });
+
+   it("Should fill username", () => {
+    cy.get('#user_login').clear();
+    cy.get('#user_login').type('username');
+   });
+
+//    it('Should fill password', () => {
+//        cy.get('input[name="user_password"]').clear();
+//        cy.get('input[name="user_password"]').type('passowrd');
+//    });
+
+//    it('Should checklist keep me signed in', () => {
+//        cy.get('input[type="checkbox"]').clear();
+//        cy.get('input[type="checkbox"]').check();
+//    });
+});
