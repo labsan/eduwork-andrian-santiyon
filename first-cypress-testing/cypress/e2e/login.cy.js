0e95f3dd43c8041cdb58c24830df87ed8d699bb0
labsan <yondeveloper@gmail.com>
2023-10-20 22:05:25 +0700

    update login test

--- first-cypress-testing/cypress/e2e/login.cy.js
+++ first-cypress-testing/cypress/e2e/login.cy.js
@@ -4,8 +4,6 @@ describe('Working with inputs', () => {
    it('Should load the website', () => {
        cy.visit('http://zero.webappsecurity.com/login.html');
        cy.url().should('include', 'login.html');
-
-      //  cy.pause();
    });
 
    it("Should fill username", () => {
@@ -22,7 +20,27 @@ describe('Working with inputs', () => {
        cy.get('input[type="checkbox"]').check();
    });
 
-   it('Should click button Sign in', () => {
+   it('Should click button sign in', () => {
       cy.get('input[name="submit"]').click();
+   });
+
+   it('Should try to login', () => {
+      cy.fixture("user").then(user => {
+         const username = user.username;
+         const password = user.password;
+
+         cy.get('#user_login').clear();
+         cy.get('#user_login').type(username);
+
+         cy.get('#user_password').clear();
+         cy.get('#user_password').type(password);
+
+         cy.get('input[type="checkbox"]').check();
+
+         cy.get('input[name="submit"]').click();
+
+         cy.get('.alert-error').should('contain.text', 'Login and/or password are wrong.');
+
+      })
    });
 });
